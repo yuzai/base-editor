8bd14f21f4693e4660a9601ce1ec1b8b73b5422a
maxiaobo01 <maxiaobo01@corp.netease.com>
2021-11-03 16:16:59 +0800

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => {
+    return (
+        <h1>
+            monaco test1
+        </h1>
+    );
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+// const monacoEditorCorePath = development ? '/Users/maxiaobo/Documents/netease/music-monaco-editor/base-editor/node_modules/monaco-editor-core/dev/vs' : '/Users/maxiaobo/Documents/netease/music-monaco-editor/base-editor/monaco-editor-core/min/vs';
+
+// const monacoEditorCorePath = '/Users/maxiaobo/Documents/netease/music-monaco-editor/base-editor/node_modules/monaco-editor/min/vs';
+// const onigasmPath = '/Users/maxiaobo/Documents/netease/music-monaco-editor/base-editor/node_modules/onigasm/lib';
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+    mode: process.env.NODE_ENV || "development",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+        // static: path.join(__dirname, "src")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         {
+        //             from: monacoEditorCorePath,
+        //             to: 'vs'
+        //         },
+        //         {
+        //             from: onigasmPath,
+        //             to: 'oniga'
+        //         },
+        //     ]
+        // }),
+        new MonacoWebpackPlugin({
+			languages: ['typescript', 'javascript', 'css']
+		}),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+};

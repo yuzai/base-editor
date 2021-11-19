fdd8df67679df4b18addbbb44b7460eaa128ebd0
maxiaobo01 <maxiaobo01@corp.netease.com>
2021-11-19 12:28:38 +0800

    fix: 发包中的自动化脚本遗漏dist文件修改的bug

--- dist/multi/Editor.d.ts
+++ dist/multi/Editor.d.ts
@@ -5,11 +5,12 @@ interface filelist {
     [key: string]: string;
 }
 declare const Editor: React.FC<{
-    path: string;
-    files: filelist;
-    value: string;
-    onValueChange: (v: string) => void;
-    onPathChange: (key: string, value: string) => void;
+    defaultPath?: string;
+    onPathChange?: (key: string, value: string) => void;
+    onValueChange?: (v: string) => void;
+    onFileChange?: (key: string, value: string) => void;
+    defaultFiles?: filelist;
+    files?: filelist;
     options: monaco.editor.IStandaloneEditorConstructionOptions;
 }>;
 export default Editor;
--- dist/single/Editor.d.ts
+++ dist/single/Editor.d.ts
@@ -1,9 +1,10 @@
 import React from 'react';
 import * as monaco from 'monaco-editor';
 declare const Editor: React.FC<{
-    value: string;
-    language: string;
-    onValueChange: (v: string) => void;
-    options: monaco.editor.IStandaloneEditorConstructionOptions;
+    defaultValue?: string;
+    value?: string;
+    language?: string;
+    onValueChange?: (v: string) => void;
+    options?: monaco.editor.IStandaloneEditorConstructionOptions;
 }>;
 export default Editor;
